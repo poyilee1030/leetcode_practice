8bb6f55c6efa6efd703f6ce6c643f07a95348311
lordleeber <poyilee1030@gmail.com>
2023-10-26 01:24:14 +0800

    Update graphs.

--- graphs/graphs/_0261_graph_valid_tree.h
+++ graphs/graphs/_0261_graph_valid_tree.h
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -10,9 +10,81 @@ using namespace std;
 class _0261_graph_valid_tree
 {
 public:
+    // translate python version
 	bool validTree(int n, vector<vector<int>>& edges) {
-		return false;
-	}
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < edges.size(); i++) {
+            vector<int> edge = edges[i];
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+
+        unordered_set<int> visited;
+        return (dfs(adj, visited, 0, -1) && n == visited.size());
+    }
+
+    bool dfs(vector<vector<int>>& adj, unordered_set<int>& visited, int i, int prev) {
+        if (visited.count(i)) {
+            return false;
+        }
+        visited.insert(i);
+        for (int j : adj[i])
+        {
+            if (j == prev)
+                continue;
+            if (!dfs(adj, visited, j, i))
+                return false;
+        }
+        return true;
+    }
+
+    /*
+    Graph of nodes, list of edges, determine if edges make valid tree
+    Ex. n = 5, edges = [[0,1],[0,2],[0,3],[1,4]] -> true
+
+    (1) For graph to be a valid tree, must have exactly n - 1 edges
+    (2) If graph fully connected & has n - 1 edges, can't contain cycle
+
+    Time: O(n)
+    Space: O(n)
+    */
+    /*
+    bool validTree(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < edges.size(); i++) {
+            vector<int> edge = edges[i];
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+        vector<bool> visited(n);
+        if (hasCycle(adj, visited, -1, 0)) {
+            return false;
+        }
+        
+        for (int i = 0; i < visited.size(); i++) {
+            if (!visited[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool hasCycle(vector<vector<int>>& adj, vector<bool>& visited, int parent, int child) {
+        if (visited[child]) {
+            return true;
+        }
+        visited[child] = true;
+        // checking for cycles and connectedness
+        for (int i = 0; i < adj[child].size(); i++) {
+            int curr = adj[child][i];
+            if (curr != parent && hasCycle(adj, visited, child, curr)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    */
 
 	void do_test(_0261_graph_valid_tree* sol)
 	{
