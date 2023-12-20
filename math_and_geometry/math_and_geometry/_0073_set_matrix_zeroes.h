ed184598737ac3daca6e23e6c1dcf860508c3624
poyilee <poyilee1030@gmail.com>
2023-12-20 12:45:33 +0800

    Update _0073_set_matrix_zeroes.h

--- math_and_geometry/math_and_geometry/_0073_set_matrix_zeroes.h
+++ math_and_geometry/math_and_geometry/_0073_set_matrix_zeroes.h
@@ -5,12 +5,67 @@
 
 using namespace std;
 
+/*
+    Given matrix, if element 0, set entire row/col to 0
+
+    Use 1st row/col as flag to determine if entire row/col 0
+
+    Time: O(mn)
+    Space: O(1)
+*/
 
 class _0073_set_matrix_zeroes
 {
 public:
 	void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
 
+        bool isFirstRowZero = false;
+        bool isFirstColZero = false;
+
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                isFirstColZero = true;
+                break;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                isFirstRowZero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (isFirstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if (isFirstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
 	}
 
 	void do_test(_0073_set_matrix_zeroes* sol) {
