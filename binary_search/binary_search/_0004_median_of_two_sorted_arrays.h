cd91949e74ed7d07c88400013c27c2f762fd774b
lordleeber <poyilee1030@gmail.com>
2024-04-07 00:21:41 +0800

    Update.

--- binary_search/binary_search/_0004_median_of_two_sorted_arrays.h
+++ binary_search/binary_search/_0004_median_of_two_sorted_arrays.h
@@ -12,111 +12,67 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if (nums1.empty()) {
             int mid_idx = nums2.size() / 2;
-            if (nums2.size() % 2 == 1) {
-                // odd
+            if (nums2.size() % 2 == 1)
                 return nums2[mid_idx];
-            }
-            else {
-                // even
+            else
                 return (nums2[mid_idx] + nums2[mid_idx - 1]) / 2.0;
-            }
         }
+
+        for (int i = 0; i < nums2.size(); i++)
+            bs(nums1, nums2[i]);
 
         int mid_idx = nums1.size() / 2;
-
-        for (int i = 0; i < nums2.size(); i++) {
-            bs(nums1, nums2[i], mid_idx);
-        }
-
-        if (nums1.size() % 2 == 1) {
-            // odd
+        if (nums1.size() % 2 == 1)
             return nums1[mid_idx];
-        }
-        else {
-            // even
+        else
             return (nums1[mid_idx] + nums1[mid_idx - 1]) / 2.0;
-        }
     }
 
-    void bs(vector<int>& nums1, int target, int& mid_idx) {
-        if (target <= nums1[0]) {
+    void bs(vector<int>& nums1, int target) {
+        if (target <= nums1[0])
             nums1.insert(nums1.begin(), target);
-            if (nums1.size() % 2 == 1) {
-                // even -> odd
-            }
-            else {
-                // odd -> even
-                mid_idx++;
-            }
-        }
-        else if (nums1.back() < target) {
+        else if (nums1.back() < target)
             nums1.push_back(target);
-            if (nums1.size() % 2 == 1) {
-                // even -> odd
-            }
-            else {
-                // odd -> even
-                mid_idx--;
-            }
-        }
         else {
-            // 1 3  t=2
-            int n = nums1.size();
             int l = 0;
-            int r = n - 1;
+            int r = nums1.size() - 1;
             while (l < r) {
-                int m = r - (r - l) / 2;
-                if (target < m) {
-                    r = m - 1;
-                }
-                else {
-                    l = m;
-                }
+                int m = l + (r - l) / 2;
+                if (target > nums1[m])
+                    l = m + 1;
+                else
+                    r = m;
             }
-
-            int mid_value = nums1[mid_idx];
             nums1.insert(nums1.begin() + l, target);
-            if (target < mid_value) {
-                if (nums1.size() % 2 == 1) {
-                    // even -> odd
-
-                }
-                else {
-                    // odd -> even
-                    // 1 2 3 4
-                    mid_idx--;
-                }
-            }
-            else {
-                if (nums1.size() % 2 == 1) {
-                    // even -> odd
-
-                }
-                else {
-                    // odd -> even
-                    mid_idx++;
-                }
-            }
         }
     }
 
 	void do_test(_0004_median_of_two_sorted_arrays* sol) {
         vector<int> nums1, nums2;
-        double ret;
+        double ret = -1;
 
         nums1 = { 1, 3 };
         nums2 = { 2 };
-        //ret = sol->findMedianSortedArrays(nums1, nums2);
-        //cout << ret << endl;
+        ret = sol->findMedianSortedArrays(nums1, nums2);
+        cout << ret << endl;
+        // Output: 2
 
         nums1 = { 1, 2 };
         nums2 = { 3, 4 };
-        //ret = sol->findMedianSortedArrays(nums1, nums2);
-        //cout << ret << endl;
+        ret = sol->findMedianSortedArrays(nums1, nums2);
+        cout << ret << endl;
+        // Output: 2.5
 
         nums1 = { 0, 0 };
         nums2 = { 0, 0 };
         ret = sol->findMedianSortedArrays(nums1, nums2);
         cout << ret << endl;
+        // Output: 0
+
+        nums1 = { 0, 0, 0, 0, 0 };
+        nums2 = { -1, 0, 0, 0, 0, 0, 1 };
+        ret = sol->findMedianSortedArrays(nums1, nums2);
+        cout << ret << endl;
+        // Output: 0
 	}
 };
