91ece87ccc49f5a638934ebc639ed52a9f01a3d7
lordleeber <poyilee1030@gmail.com>
2024-04-18 01:43:56 +0800

    Update.

--- heap_priority_queue/heap_priority_queue/_0621_task_scheduler.h
+++ heap_priority_queue/heap_priority_queue/_0621_task_scheduler.h
@@ -3,46 +3,80 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+
 using namespace std;
 
 
 class _0621_task_scheduler
 {
 public:
+	/* leetcode: 48ms
 	int leastInterval(vector<char>& tasks, int n) {
+		unordered_map<char, int>Map;
+		for (auto ch : tasks)
+			Map[ch]++;
+
+		int maxFreq = 0;
+		for (auto a : Map)
+			maxFreq = max(maxFreq, a.second);
+
+		int result = (maxFreq - 1) * (n + 1);
+		int count = 0;
+		for (auto a : Map)
+		{
+			if (a.second == maxFreq)
+				count++;
+		}
+		return max(result + count, (int)tasks.size());
+	}
+	*/
+
+	// 全模擬，較好記，leetcode: 82ms
+	int leastInterval(vector<char>& tasks, int n) {
+		unordered_map<char, int> freq;
+		for (char ch : tasks) {
+			freq[ch]++;
+		}
+
+		n = n + 1;
+
 		priority_queue<int> pq;
-		queue<vector<int>> q;
-		vector<int> counter(26);
-
-		for (int i = 0; i < tasks.size(); ++i)
-			++counter[tasks[i] - 'A'];
-		for (int i = 0; i < 26; ++i) {
-			if (counter[i])
-				pq.push(counter[i]);
+		for (auto it = freq.begin(); it != freq.end(); it++) {
+			pq.push(it->second);
 		}
 
-		int time = 0;
-		while (!q.empty() || !pq.empty()) {
-			++time;
-			if (!pq.empty()) {
-				if (pq.top() - 1)
-					q.push({ pq.top() - 1, time + n });
+		int count = 0;
+		while (!pq.empty()) {
+			int k = min((int)pq.size(), n);
+			vector<int> tmp;
+			for (int i = 0; i < k; i++) {
+				int f = pq.top();
 				pq.pop();
+				f--;
+				if (f > 0)
+					tmp.push_back(f);
 			}
-			if (!q.empty() && q.front()[1] == time) {
-				pq.push(q.front()[0]);
-				q.pop();
-			}
+
+			if (tmp.size() > 0)
+				count += n;
+			else
+				count += k;
+
+			for (auto x : tmp)
+				pq.push(x);
 		}
-		return time;
+
+		return count;
 	}
 
 	void do_test(_0621_task_scheduler *sol)
 	{
-		vector<char> tasks1 = { 'A', 'A', 'A', 'B', 'B', 'B' };
-		int n1 = 2;
-		int res1 = sol->leastInterval(tasks1, n1);
-		cout << res1 << endl;;
+		vector<char> tasks;
+		int ret;
+
+		tasks = { 'A', 'A', 'A', 'B', 'B', 'B' };
+		ret = sol->leastInterval(tasks, 2);
+		cout << ret << endl;;
 		/*
 		Output : 8
 		Explanation :
@@ -50,10 +84,9 @@ public:
 		There is at least 2 units of time between any two same tasks.
 		*/
 
-		vector<char> tasks2 = { 'A', 'A', 'A', 'B', 'B', 'B' };
-		int n2 = 0;
-		int res2 = sol->leastInterval(tasks2, n2);
-		cout << res2 << endl;;
+		tasks = { 'A', 'A', 'A', 'B', 'B', 'B' };
+		ret = sol->leastInterval(tasks, 0);
+		cout << ret << endl;;
 		/*
 		Output : 6
 		Explanation : On this case any permutation of size 6 would work since n = 0.
@@ -64,10 +97,9 @@ public:
 		And so on.
 		*/
 		
-		vector<char> tasks3 = { 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
-		int n3 = 2;
-		int res3 = sol->leastInterval(tasks3, n3);
-		cout << res3 << endl;
+		tasks = { 'A', 'A', 'A', 'A', 'A', 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+		ret = sol->leastInterval(tasks, 2);
+		cout << ret << endl;
 		/*
 		Output : 16
 		Explanation :
--- heap_priority_queue/heap_priority_queue/heap_priority_queue.cpp
+++ heap_priority_queue/heap_priority_queue/heap_priority_queue.cpp
@@ -18,11 +18,11 @@ int main()
     //_0215_kth_largest_element_in_an_array sol;
     //_0295_find_median_from_data_stream sol;
     //_0355_design_twitter sol;
-    //_0621_task_scheduler sol;
+    _0621_task_scheduler sol;
     //_0703_kth_largest_element_in_a_stream sol;
     //_0973_k_closest_points_to_origin sol;
     //_1046_last_stone_weight sol;
-    _1642_furthest_building_you_can_reach sol;
+    //_1642_furthest_building_you_can_reach sol;
     //_1845_seat_reservation_manager sol;
     //_1921_eliminate_maximum_number_of_monsters sol;
 
