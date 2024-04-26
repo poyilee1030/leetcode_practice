6b288caa9416929d79bd2149b9f5a244b812c864
lordleeber <poyilee1030@gmail.com>
2024-04-27 00:31:00 +0800

    Update.

--- advanced_graphs/advanced_graphs/_0332_reconstruct_itinerary.h
+++ advanced_graphs/advanced_graphs/_0332_reconstruct_itinerary.h
@@ -3,43 +3,53 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <queue>
+#include "utils.h"
 
 using namespace std;
 
 
 class _0332_reconstruct_itinerary
 {
+    unordered_map<string, queue<string>> route;
 public:
-	vector<string> findItinerary(vector<vector<string>>& tickets) {
-		vector<string> a = { "a" };
-		return a;
-	}
+    vector<string> findItinerary(vector<vector<string>> tickets)
+    {
+        sort(tickets.begin(), tickets.end());
+        for (auto ticket: tickets)
+            route[ticket[0]].push(ticket[1]);
+
+        vector<string> path;
+        dfs("JFK", path);
+        reverse(path.begin(), path.end());
+        return path;
+    }
+
+    void dfs(string start, vector<string>& path) {
+        while (route[start].size() > 0) {
+            string nextStart = route[start].front();
+            route[start].pop();
+            dfs(nextStart, path);
+        }
+        path.push_back(start);
+    }
 
 	void do_test(_0332_reconstruct_itinerary* sol)
 	{
-		vector<vector<string>> tickets1 = { {"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"} };
-		vector<string> ret1 = sol->findItinerary(tickets1);
-		print_res(ret1);
+		vector<vector<string>> tickets;
+		vector<string> ret;
+
+		tickets = { {"MUC", "LHR"}, {"JFK", "MUC"}, {"SFO", "SJC"}, {"LHR", "SFO"} };
+		ret = sol->findItinerary(tickets);
+		print_vector(ret);
 		// Output : ["JFK", "MUC", "LHR", "SFO", "SJC"]
 
-		vector<vector<string>> tickets2 = { {"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"}, {"ATL", "JFK"}, {"ATL", "SFO"} };
-		vector<string> ret2 = sol->findItinerary(tickets2);
-		print_res(ret2);
+		tickets = { {"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"}, {"ATL", "JFK"}, {"ATL", "SFO"} };
+		ret = sol->findItinerary(tickets);
+		print_vector(ret);
 		// Output : ["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"]
 		// Explanation : Another possible reconstruction is["JFK", "SFO", "ATL", "JFK", "ATL", "SFO"] 
 		// but it is larger in lexical order.
 	}
-
-	void print_res(vector<string>& result)
-	{
-		cout << "[";
-		for (int i = 0; i < result.size(); i++)
-		{
-			cout << result[i];
-			if (i < result.size() - 1)
-				cout << ",";
-		}
-		cout << "]" << endl;
-	}
 };
-
