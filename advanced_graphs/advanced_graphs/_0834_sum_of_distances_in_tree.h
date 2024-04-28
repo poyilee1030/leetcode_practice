3e46784b0e7df9fe6482817af6417d5d337a3142
lordleeber <poyilee1030@gmail.com>
2024-04-28 22:36:01 +0800

    Update.

--- advanced_graphs/advanced_graphs/_0834_sum_of_distances_in_tree.h
+++ advanced_graphs/advanced_graphs/_0834_sum_of_distances_in_tree.h
@@ -5,6 +5,12 @@
 
 using namespace std;
 
+/*
+VC的預設 stack reserve size 只有 1 MB
+從 Project -> Properties (最下面) -> Configuration Properties -> Linker
+              System -> Stack reserve size -> 填入 4194304 (4 MB)
+
+*/
 
 class _0834_sum_of_distances_in_tree
 {
@@ -67,6 +73,14 @@ public:
         }
     }
 
+    void clear() {
+        memset(visited, 0, 30005 * 4);
+        memset(count, 0, 30005 * 4);
+        next->clear();
+        
+    }
+
+
 	void do_test(_0834_sum_of_distances_in_tree* sol) {
         vector<vector<int>> edges;
         vector<int> ret;
@@ -74,6 +88,18 @@ public:
         edges = { {0,1}, {0,2}, {2,3}, {2,4}, {2,5} };
         ret = sol->sumOfDistancesInTree(6, edges);
         print_vector(ret);
-        // Outupt: [8,12,6,10,10,10]
+        // Outupt: [8, 12, 6, 10, 10, 10]
+        sol->clear();
+
+        edges = {};
+        ret = sol->sumOfDistancesInTree(1, edges);
+        print_vector(ret);
+        // Outupt: [0]
+        sol->clear();
+
+        edges = { {1,0} };
+        ret = sol->sumOfDistancesInTree(2, edges);
+        print_vector(ret);
+        // Outupt: [1, 1]
 	}
 };
