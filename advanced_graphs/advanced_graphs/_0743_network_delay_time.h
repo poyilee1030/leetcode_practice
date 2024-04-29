239375eafd586444954073cf15100ac7fa4790e9
lordleeber <poyilee1030@gmail.com>
2024-04-30 01:34:35 +0800

    Update.

--- advanced_graphs/advanced_graphs/_0743_network_delay_time.h
+++ advanced_graphs/advanced_graphs/_0743_network_delay_time.h
@@ -2,38 +2,70 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
+typedef pair<int, int> PII;
 
 class _0743_network_delay_time
 {
 public:
-	int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-		return 0;
-	}
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<PII>> route(n + 1);
+        for (auto t : times)
+            route[t[0]].push_back( {t[1], t[2]});
+
+        priority_queue<PII, vector<PII>, greater<PII>> pq;
+        pq.push({ 0, k });
+        vector<int>resolved(n + 1, 0);
+        int ans = 0;
+
+        while (!pq.empty()) {
+            auto x = pq.top();
+            int d = x.first;
+            int cur = x.second;
+            pq.pop();
+
+            if (resolved[cur] == 1)
+                continue;
+            resolved[cur] = 1;
+            ans = max(ans, d);  // dist[cur] = d
+
+            for (auto y: route[cur]) {
+                int nxt = y.first;
+                int weight = y.second;
+                if (resolved[nxt])
+                    continue;
+                pq.push({ d + weight, nxt });
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+            if (resolved[i] == 0)
+                return -1;
+
+        return ans;
+    }
 
 	void do_test(_0743_network_delay_time* sol)
 	{
-		vector<vector<int>> times1 = { {2, 1, 1}, {2, 3, 1}, {3, 4, 1} };
-		int n1 = 4;
-		int k1 = 2;
-		int ret1 = sol->networkDelayTime(times1, n1, k1);
-		cout << ret1 << endl;
+        vector<vector<int>> times;
+        int ret;
+		
+        times = { {2, 1, 1}, {2, 3, 1}, {3, 4, 1} };
+        ret = sol->networkDelayTime(times, 4, 2);
+		cout << ret << endl;
 		// Output : 2
 
-		vector<vector<int>> times2 = { {1, 2, 1} };
-		int n2 = 2;
-		int k2 = 1;
-		int ret2 = sol->networkDelayTime(times2, n2, k2);
-		cout << ret2 << endl;
+		times = { {1, 2, 1} };
+		ret = sol->networkDelayTime(times, 2, 1);
+		cout << ret << endl;
 		// Output : 1
 
-		vector<vector<int>> times3 = { {1, 2, 1} };
-		int n3 = 2;
-		int k3 = 2;
-		int ret3 = sol->networkDelayTime(times3, n3, k3);
-		cout << ret3 << endl;
+		times = { {1, 2, 1} };
+		ret = sol->networkDelayTime(times, 2, 2);
+		cout << ret << endl;
 		// Output : -1
 	}
 };
--- advanced_graphs/advanced_graphs/advanced_graphs.cpp
+++ advanced_graphs/advanced_graphs/advanced_graphs.cpp
@@ -16,10 +16,10 @@ int main()
 {
     //_0269_alien_dictionary sol;
     //_0332_reconstruct_itinerary sol;
-    //_0743_network_delay_time sol;
+    _0743_network_delay_time sol;
     //_0778_swim_in_rising_water sol;
     //_0787_cheapest_flights_within_k_stops sol;
-    _0834_sum_of_distances_in_tree sol;
+    //_0834_sum_of_distances_in_tree sol;
     //_1584_min_cost_to_connect_all_points_kruskal sol;
     //_1584_min_cost_to_connect_all_points_prim sol;
     //_2092_find_all_people_with_secret sol;
