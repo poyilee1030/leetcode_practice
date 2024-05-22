80e35b96e8999698c603b0593773f80a8d5cb4e3
lordleeber <poyilee1030@gmail.com>
2024-05-23 02:10:28 +0800

    Update.

--- backtracking/backtracking/_0047_permutations_ii.h
+++ backtracking/backtracking/_0047_permutations_ii.h
@@ -34,7 +34,11 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             if (visited[i])
                 continue;
-            if (i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) // <--新增
+            
+            // 若與前一個位置的數相同，且前一個位置還沒拜訪過，則當前不能先拜訪
+            // 如何保證取數不重複? 約定相同的數如果要取，就一定要從前面開始取
+            // 可看官神youtube 40. Combination Sum II, 2/3/2020 約 6分鐘位置
+            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) // <--新增
                 continue;
             curr.push_back(nums[i]);
             visited[i] = 1;
